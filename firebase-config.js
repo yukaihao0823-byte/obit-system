08bf609ec8519533c74e0bdd85aaf0c577d2e123
yukaihao0823-byte <yu.kaihao.0823@gmail.com>
2026-05-18 22:51:01 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyDSIlx0g8v6BFgMO2c8TQQs8b4dyl",
+  authDomain:        "obit-system-ec116.firebaseapp.com",
+  projectId:         "obit-system-ec116",
+  storageBucket:     "obit-system-ec116.firebasestorage.app",
+  messagingSenderId: "973208853887",
+  appId:             "1:973208853887:web:7acd2926e17f129"
+};
+
+const COMPANY_NAME = "醬醬興有限公司";
+
+const OBITUARY_BASE_URL = "obituary.html";
